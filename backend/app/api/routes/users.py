3916660811d766dc1060e1a9e4971aef3e1f1d55
S4ske <sany1111.ru@mail.ru>
2024-08-26 10:22:46 +0500

    Add GET and POST in users.py

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,3 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from ..deps import SessionDep
+from uuid import UUID
+from ...crud import get_user_by_id, create_user, get_user_by_email
+from ...models import UserPublic, UserCreate
 
-router = APIRouter()
+router = APIRouter(tags=['users'])
+
+
+@router.post('/')
+async def create_user(db_session: SessionDep, user_create: UserCreate) -> UserPublic:
+    user = get_user_by_email(db_session, user_create.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='The user with this email already exists in the system.'
+        )
+    user_db = create_user(db_session, user_create)
+    user_public = UserPublic.model_validate(user_db)
+    return user_public
+
+
+@router.get('/{id}')
+async def get_user(db_session: SessionDep, id: UUID) -> UserPublic:
+    user_db = get_user_by_id(db_session, id)
+    user_public = UserPublic.model_validate(user_db)
+    return user_public
