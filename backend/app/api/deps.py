92a56f611c27edaad9d31abb11a018093e7ce169
S4ske <sany1111.ru@mail.ru>
2024-08-26 10:06:36 +0500

    Add SessionDep

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,8 +1,12 @@
 from ..core.db import engine
 from sqlmodel import Session
-from typing import Generator
+from typing import Generator, Annotated
+from fastapi import Depends
 
 
 def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
+
+
+SessionDep = Annotated[Session, Depends(get_db)]
