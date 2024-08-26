468d316633e1c9a0558ba4556423878132a9cc19
S4ske <sany1111.ru@mail.ru>
2024-08-26 10:26:31 +0500

    Include api_router into app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
+from api.main import api_router
 
 app = FastAPI()
+
+app.include_router(api_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
