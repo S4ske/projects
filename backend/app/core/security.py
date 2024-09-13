2ad9c73f0a6331483fa619479447780a9075e70e
S4ske <sany1111.ru@mail.ru>
2024-09-13 09:45:25 +0500

    Add authenticate func

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,11 +3,14 @@ from datetime import timedelta, datetime, timezone
 from typing import Any
 import jwt
 from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
+from backend.app.models import User
+from backend.app.crud import get_user_by_username
 
 crypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
-def create_access_token(subject: str | Any, expires_delta: timedelta):
+def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
     expire = datetime.now(timezone.utc) + (expires_delta if expires_delta else timedelta(minutes=30))
     to_encode = {'exp': expire, 'sub': subject}
     encoded_jwt = jwt.encode(to_encode, algorithm=JWT_ALGORITHM, key=JWT_SECRET_KEY)
@@ -20,3 +23,12 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return crypt_context.hash(password)
+
+
+async def authenticate(db_session: AsyncSession, username: str, password: str) -> User | None:
+    user = await get_user_by_username(db_session, username)
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
